126b5af4c66bbbf2394efde633952805d77e98df
Christopher LoPresti <lopresti.chris@yahoo.com>
2024-08-16 17:50:29 -0400

    cors issue to backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const { createClient } = require("redis");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000/" }));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookies());
